98e0e93de97ab73821d9331ca1428dad1277e4ff
Julio Sandro <multyroom@gmail.com>
2022-07-08 14:04:29 -0400

    ajuste de diretorio

--- frontend/src/component/home/home.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import Main from "../templates/main";
-
-export default props =>
-    <Main>
-        <div className="display-4">Bem Vindo!</div>
-        <hr />
-        <p className="mb-0">Sistema teste</p>
-    </Main>
--- frontend/src/component/templates/header.jsx
+++
@@ -1,9 +0,0 @@
-import './header.css'
-import React from 'react'
-import cart from '../../assets/cart-icon.svg'
-
-
-export default props =>
-    <header className="cart d-none d-sm-flex flex-column">
-        E-Commerce
-    </header>
